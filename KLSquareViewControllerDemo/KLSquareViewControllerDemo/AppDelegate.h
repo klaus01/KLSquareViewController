ebde6c44f92ed0ae75c3bc8165b657edfd180e16
klaus01 <kelei0017@gmail.com>
2013-04-27 21:44:58 +0800

    Create demo project

---
+++ KLSquareViewControllerDemo/KLSquareViewControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KLSquareViewControllerDemo
+//
+//  Created by 柯磊 on 13-4-27.
+//  Copyright (c) 2013年 柯磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KLSquareViewControllerDemo/KLSquareViewControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KLSquareViewControllerDemo
+//
+//  Created by 柯磊 on 13-4-27.
+//  Copyright (c) 2013年 柯磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
